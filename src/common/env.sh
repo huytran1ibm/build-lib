2ad9928ace29202a799729439007f1aeb3ced496
Rajesh Nilam <rajesh@oc8338168485.ibm.com>
2021-04-21 21:47:35 +0530

    update code as huy

--- src/common/env.sh
+++ src/common/env.sh
@@ -11,9 +11,9 @@ export NVM_VERSION=${NVM_VERSION:="0.35.1"}
 
 # set location for go executables
 export GO_VERSION=${GO_VERSION:="1.13"}
-export GOROOT=${ROOTDIR}/go
+export GOROOT=${GOROOT:-"${ROOTDIR}/go"}
 export PATH=${GOROOT}/bin:$PATH
-export GOPATH=${ROOTDIR}/go
+export GOPATH=${GOPATH:-"${ROOTDIR}/go"}
 export PATH=${GOPATH}/bin:$PATH
 export ENABLE_PEER_CLI=true
 
@@ -36,8 +36,8 @@ export FABRIC_SRC_DIR=${ROOTDIR}/fabric-${HLF_VERSION}
 export FABRIC_CLI_DIR=$ROOTDIR/${FABRIC_CLI_DIR:="/fabric-cli"}
 
 ## Fabric V2.x Env setup
-if [[ $HLF_VERSION == "2."* && $ENABLE_PEER_CLI == 'true' ]];then
-    echo " checking peerclie trie or not"
+if [[ $HLF_VERSION == "1."* && $ENABLE_PEER_CLI == 'true' ]] || [[ $HLF_VERSION == "2."* && $ENABLE_PEER_CLI == 'true' ]];then
+    if [[ $DEBUG == 'true' ]];then set -x; fi;
     echo "-------- Installing jq --------"
     install_jq
 
@@ -78,12 +78,16 @@ if [[ $HLF_VERSION == "2."* && $ENABLE_PEER_CLI == 'true' ]];then
     ca=$(echo "${ADMIN_IDENTITY_STRING}" | jq -r '.ca')
     key=$(echo "${ADMIN_IDENTITY_STRING}" | jq -r '.private_key')
     name=$(echo "${ADMIN_IDENTITY_STRING}" | jq -r '.name')
-    ./${SCRIPT_DIR}/common/create_msp_from_identity.sh "${ROOTDIR}/${ADMIN_IDENTITY_NAME}" "${cert}" "${ca}" "${key}" "${name}"
+
+    source ${SCRIPT_DIR}/common/create_msp_from_identity.sh "${ROOTDIR}/${ADMIN_IDENTITY_NAME}" "${cert}" "${ca}" "${key}" "${name}"
 #    ./${SCRIPT_DIR}/common/create_msp_from_identity.sh "${ROOTDIR}/${ADMIN_IDENTITY_NAME}" "${cert}" "${key}" "${name}"
 
     # Download Fabric BIN and setup PEER's core.yaml for identity
-    install_fabric_bin
-    cp $FABRIC_CFG_PATH/core.yaml "${ROOTDIR}/${ADMIN_IDENTITY_NAME}"
+    install_fabric_bin "${HLF_VERSION}" "1.4.9" # ca 1.4.9 is latest
+    ## Check if dir exists
+    [[ ! -d "${ROOTDIR}/${ADMIN_IDENTITY_NAME}" ]] && exit 6
+
+    cp $(pwd)/config/core.yaml "${ROOTDIR}/${ADMIN_IDENTITY_NAME}/"
 
     #Extract env
     #Get Msp
@@ -112,7 +116,7 @@ if [[ $HLF_VERSION == "2."* && $ENABLE_PEER_CLI == 'true' ]];then
         echo $peerObj | jq -r '.pem' > ${peerPemFile}
         # create map
         peersMap["${peerUrl}"]=$(pwd)/${peerPemFile}
-    done < <(echo ${CONNECTION_PROFILE_STRING} | jq -rc '.[0] | .peers | keys[] as $k | {"url": "\(.[$k] | .url)" , "pem": "\(.[$k] | .tlsCACerts.pem)"}')
+    done < <(echo "${CONNECTION_PROFILE_STRING}" | jq -rc '.[0] | .peers | keys[] as $k | {"url": "\(.[$k] | .url)" , "pem": "\(.[$k] | .tlsCACerts.pem)"}')
     export peersMap
 
     ## Build the peer string for peer cli
@@ -123,19 +127,23 @@ if [[ $HLF_VERSION == "2."* && $ENABLE_PEER_CLI == 'true' ]];then
 
     # Note: chaincode level shouldn't be array as deploy_config.json is a specific chaincode configuration for distinct source code deployment
     # TODO Allow CC_NAME override at pipeline
-    CC_NAME=$(cat $CONFIGPATH | jq -r '. | .. | .chaincode? | .[0] | .name | select(.)')
+    if [[ ! -z "${CC_NAME_OVERRIDE}" ]];then
+        CC_NAME=${CC_NAME_OVERRIDE}
+    else
+        CC_NAME=$(cat $CONFIGPATH | jq -r --argjson cc_index $CC_INDEX '. | .. | .chaincode? | .[$cc_index] | .name | select(.)')
+    fi
 
-    json_version=$(cat $CONFIGPATH | jq -r '. | .. | .chaincode? | .[0] | .version? | select(.)')
+    json_version=$(cat $CONFIGPATH | jq -r --argjson cc_index $CC_INDEX  '. | .. | .chaincode? | .[$cc_index] | .version? | select(.)')
     if [[ $json_version != null && $json_version != "" ]]; then
         CC_VERSION=$json_version
     else
         CC_VERSION="${CC_VERSION_OVERRIDE:-latest}"
     fi
     #TODO enable multiple channel
-    CHANNEL_NAME=$(cat $CONFIGPATH | jq -r '. | .. | .chaincode? | .[0] | .channel | select(.)')
+    CHANNEL_NAME=$(cat $CONFIGPATH | jq -r --argjson cc_index $CC_INDEX  '. | .. | .chaincode? | .[$cc_index] | .channel | select(.)')
 
     ##PDC
-    pdc_json_path=$(cat $CONFIGPATH | jq -r '. | .. | .chaincode? | .[0] | .pdc_path? | select(.)')
+    pdc_json_path=$(cat $CONFIGPATH | jq -r --argjson cc_index $CC_INDEX  '. | .. | .chaincode? | .[$cc_index] | .pdc_path? | select(.)')
 
     if [[ $pdc_json_path != null && $pdc_json_path != "" ]]; then
         CC_PDC_CONFIG="--collections-config ${CC_REPO_DIR}/${pdc_json_path}"
@@ -149,6 +157,22 @@ if [[ $HLF_VERSION == "2."* && $ENABLE_PEER_CLI == 'true' ]];then
         export CC_SIGNATURE_OPTION=""
     else
         export CC_SIGNATURE_OPTION=--signature-policy
+    fi
+
+    ## Endorsement policy should be defined at cicd by admin
+    if [[ -z $ENDORSEMENT_POLICY ]];then
+        ENDORSEMENT_POLICY=""
+        export CC_ENDORSEMENT_OPTION=""
+    else
+        export CC_ENDORSEMENT_OPTION=--policy
+    fi
+
+    ## Init Constructor if needed
+    if [[ -z $INIT_ARGS ]];then
+        INIT_ARGS=""
+        export CC_INIT_ARGS_OPTION=""
+    else
+        export CC_INIT_ARGS_OPTION=--ctor
     fi
 
     export CC_PDC_CONFIG=${CC_PDC_CONFIG}
--- src/go-chaincode/package.sh
+++ src/go-chaincode/package.sh
@@ -18,22 +18,23 @@ verifyPeerEnv
 #rm -rf "${CC_REPO_DIR}/node_modules"
 
 # Find an existing cc namespace
-queryCommitted
-if [[ -z $LATEST_SEQ ]];then
-    # Did not find last seq therefore define
-    export LATEST_SEQ=0
+if [[ "${HLF_VERSION}" == "2."* ]]; then
+  queryCommitted
+  if [[ -z $LATEST_SEQ ]];then
+      # Did not find last seq therefore define
+      export LATEST_SEQ=0
+  fi
+  #rm last entry
+  awk '!/LATEST_SEQ/' build.properties > temp && mv temp build.properties
+  echo "LATEST_SEQ=${LATEST_SEQ}" >> build.properties
+  export CC_SEQUENCE=${CC_SEQUENCE_OVERRIDE:-$(expr $LATEST_SEQ + 1)}
 fi
 
-#rm last entry
-awk '!/LATEST_SEQ/' build.properties > temp && mv temp build.properties
-echo "LATEST_SEQ=${LATEST_SEQ}" >> build.properties
-
-export CC_SEQUENCE=${CC_SEQUENCE_OVERRIDE:-$(expr $LATEST_SEQ + 1)}
-
 # Update package.json cc name and version
-#cd "${CC_REPO_DIR}"
+cd "${CC_REPO_DIR}"
+##Update CC_VERSION for go package if any
 #npm version prerelease --preid="${CC_VERSION}"
-#cd -
+cd -
 
 packageCC "${CC_REPO_DIR}" "${CC_NAME}" "${CC_VERSION}" "${CC_SEQUENCE}" "golang"
 
