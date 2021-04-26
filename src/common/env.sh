e693beb70090dcceeb80cc49a3b5b9fc04c94e13
Rajesh Nilam <rajesh@oc8338168485.ibm.com>
2021-04-26 14:38:36 +0530

    updated GOPATH

--- src/common/env.sh
+++ src/common/env.sh
@@ -11,9 +11,9 @@ export NVM_VERSION=${NVM_VERSION:="0.35.1"}
 
 # set location for go executables
 export GO_VERSION=${GO_VERSION:="1.13"}
-export GOROOT=${ROOTDIR}/go
+export GOROOT=${GOROOT:-"${ROOTDIR}/go"}
 export PATH=${GOROOT}/bin:$PATH
-export GOPATH=${ROOTDIR}
+export GOPATH=${GOPATH:-"${ROOTDIR}/go"}
 export PATH=${GOPATH}/bin:$PATH
 export ENABLE_PEER_CLI=true
 
