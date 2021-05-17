d7e8e8a0fa6c8a2426301a7f2fad954fea9e6b3e
Rajesh Nilam <rajesh@oc8338168485.ibm.com>
2021-05-17 15:24:23 +0530

    updated deploy_v2.sh

--- src/go-chaincode/deploy_v2.sh
+++ src/go-chaincode/deploy_v2.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 export ENABLE_PEER_CLI=true
 # Common deploy script for chaincode
 # shellcheck source=src/common/utils.sh
@@ -9,8 +8,6 @@ source "${SCRIPT_DIR}/common/env.sh"
 # shellcheck source=src/common/blockchain.sh
 source "${SCRIPT_DIR}/common/blockchain.sh"
 export CC_SEQUENCE=${CC_SEQUENCE_OVERRIDE:-$(expr $LATEST_SEQ + 1)}
-
-
 : "${CC_REPO_DIR:?"CC_REPO_DIR not set" }"
 : "${CC_NAME:?"CC_NAME not set" }"
 : "${CC_VERSION:?"CC_VERSION not set" }"
